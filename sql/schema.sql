af4bdf5f7df226550a13dc895c22cdd231e1b8ef
jackcase04 <jackcase04@gmail.com>
2025-10-09 16:19:46 -0500

    fixed some typos

--- sql/schema.sql
+++ sql/schema.sql
@@ -2,7 +2,7 @@
 PRAGMA foreign_keys = ON;
 
 CREATE TABLE IF NOT EXISTS users (
-    id INTEGER PRIMARY KEY,
+    user_id INTEGER PRIMARY KEY,
     email TEXT,
     password TEXT,
     name TEXT,
@@ -10,31 +10,31 @@ CREATE TABLE IF NOT EXISTS users (
 );
 
 CREATE TABLE IF NOT EXISTS authors (
-    id INTEGER PRIMARY KEY,
+    author_id INTEGER PRIMARY KEY,
     name TEXT -- TODO: figure out how to have first middle and last
     -- TODO: add relations between authors and books
 );
 
 -- TODO: How can multiple admins be the contact for something?
 CREATE TABLE IF NOT EXISTS books (
-    id INTEGER PRIMARY KEY,
+    book_id INTEGER PRIMARY KEY,
     title TEXT,
     edition TEXT,
     condition TEXT,
     additional_info TEXT,
     checked_out BOOLEAN DEFAULT 0,
-    FOREIGN KEY (contact) REFRENCES admin(id),
+    FOREIGN KEY (contact) REFERENCES users(user_id),
     -- Book can be checked out only by one user at a time
-    FOREIGN KEY (checked_out_by) REFRENCES users(id)
+    FOREIGN KEY (checked_out_by) REFERENCES users(user_id)
 );
 
 CREATE TABLE IF NOT EXISTS equipment (
-    id INTEGER PRIMARY KEY,
+    equipment_id INTEGER PRIMARY KEY,
     name TEXT,
     class_requirement TEXT,
     checked_out BOOLEAN DEFAULT 0,
     additional_info TEXT,
-    FOREIGN KEY (contact) REFRENCES admin(id),
+    FOREIGN KEY (contact) REFERENCES users(user_id),
     -- Equipment can be checked out only by one user at a time
-    FOREIGN KEY (checked_out_by) REFRENCES users(id)
+    FOREIGN KEY (checked_out_by) REFERENCES users(user_id)
 )
