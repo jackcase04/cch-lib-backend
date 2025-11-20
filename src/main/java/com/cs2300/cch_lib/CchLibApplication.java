9b3354c1d548a99ef847620c52cec910226308eb
jackcase04 <jackcase04@gmail.com>
2025-11-20 16:12:09 -0600

    added spring start project

---
+++ src/main/java/com/cs2300/cch_lib/CchLibApplication.java
@@ -0,0 +1,13 @@
+package com.cs2300.cch_lib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CchLibApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CchLibApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cs2300/cch_lib/CchLibApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cs2300.cch_lib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CchLibApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
